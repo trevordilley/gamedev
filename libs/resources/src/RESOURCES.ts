74c33d30d2e3f536b2e548877d87f4259aefa272
Trevor Dilley <trevor.lee.dilley@gmail.com>
2023-10-07 21:38:29 -0700

    Example of how this resources file might be shaped.

--- libs/resources/src/RESOURCES.ts
+++ libs/resources/src/RESOURCES.ts
@@ -3,5 +3,18 @@ import {knightResource} from "./resourceLogicks/knight";
 
 
 export const RESOURCES = (scene: Scene, baseUrl: string) => ({
-  KNIGHT: (id?: string) => knightResource(scene, baseUrl, id)
+  // NPCs
+  KNIGHT: (id?: string) => knightResource(scene, baseUrl, id),
+  GOBLIN: (id?: string) => knightResource(scene, baseUrl, id),
+  COMMONER: (id?: string) => knightResource(scene, baseUrl, id),
+
+  // Doodads
+  PINE: (id?: string) => knightResource(scene, baseUrl, id),
+  BIG_TREASURE: (id?: string) => knightResource(scene, baseUrl, id),
+  SNOW_BERRY_BUSH: (id?: string) => knightResource(scene, baseUrl, id),
+
+  // Backgrounds
+  CAVE: (id?: string) => knightResource(scene, baseUrl, id),
+  WITCH_HUT: (id?: string) => knightResource(scene, baseUrl, id),
+  CASTLE: (id?: string) => knightResource(scene, baseUrl, id),
 })
