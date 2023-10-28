9b554c729992c8fcd4916a3ede7685ac58aa00f9
Trevor Dilley <trevor.lee.dilley@gmail.com>
2023-10-27 21:11:58 -0700

    broke out the project parser into it's own file

--- libs/hyperlap2d/parser/src/lib/libs/hyperlap2d/parser.ts
+++ libs/hyperlap2d/parser/src/lib/libs/hyperlap2d/parser.ts
@@ -1,56 +1,6 @@
 import * as zod from "zod"
 import {HyperLap2DScene, HyperLap2DSceneSchema} from "./parser.scene";
-const OriginalResolutionSchema = zod.object({
-  name: zod.string(),
-  width: zod.number(),
-  height: zod.number()
-})
-
-const SceneSchema = zod.object(
-  {
-    sceneName: zod.string(),
-    physicsPropertiesVO: zod.object({}),
-    lightsPropertiesVO: zod.object({}),
-    shaderVO: zod.object({})
-  })
-
-
-const LibraryActionSchema =
-  zod.object({
-      version: zod.number(),
-      nodes: zod.object({
-        id: zod.string(),
-        type: zod.string(),
-        x: zod.number(),
-        y: zod.number(),
-        data: zod.record(zod.string(), zod.string()).optional()
-
-      }).array(),
-      connections: zod.object({
-        fromNode: zod.string(),
-        fromField: zod.string(),
-        toNode: zod.string(),
-        toField: zod.string()
-      }).array()
-    }
-  )
-
-const ImagePackSchema = zod.object({
-  name: zod.string(),
-  regions: zod.string().array().optional()
-})
-
-const HyperLap2DProjectSchema = zod.object({
-  originalResolution: OriginalResolutionSchema,
-  scenes: SceneSchema.array(),
-  libraryActions: zod.record( zod.string(),LibraryActionSchema).optional(),
-  imagesPacks: zod.record(zod.string(), ImagePackSchema),
-  animationsPacks: zod.record(zod.string(), zod.object({
-    name: zod.string(),
-    regions: zod.string().array().optional()
-  }))})
-
-type HyperLap2DProject = zod.infer<typeof HyperLap2DProjectSchema>
+import {HyperLap2DProject, HyperLap2DProjectSchema} from "./parser.project";
 
 interface HyperLap2D {
   project: HyperLap2DProject
